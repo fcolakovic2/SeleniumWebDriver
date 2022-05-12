b043e65b1045644c26d972a35b1d1dafe2821337
fcolakovic2 <FarisColakovic@FARIS>
2022-05-12 09:19:34 +0200

    Testing valid data input

---
+++ SeleniumCourseFinal/Scenarios/RegisterValidData.cs
@@ -0,0 +1,61 @@
+﻿namespace SeleniumCourseFinal
+{
+    using OpenQA.Selenium;
+    using NUnit.Framework;
+
+    public class RegisterValidData
+    {
+        IAlert alert;
+
+        public RegisterValidData()
+        {
+        }
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            Actions.InitializeDriver();
+            NavigateTo.RegistrationFormThroughTestScenarios();
+        }
+
+        [TestCase]
+        public void CorrectData()
+        {
+
+            Actions.FillRegistrationForm(
+                "111111",
+                "password",
+                "Faris",
+                "Adresa",
+                 1,
+                "71000",
+                "fariscolakovic@icloud.com",
+                1,
+                true,
+                "description"
+                );
+
+
+            try
+            {
+                Driver.driver.SwitchTo().Alert();
+                Assert.Fail();
+            }
+            catch (NoAlertPresentException ex)
+            {
+                Assert.Pass();
+            }
+
+
+        }
+
+
+
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            Driver.driver.Quit();
+        }
+    }
+}
